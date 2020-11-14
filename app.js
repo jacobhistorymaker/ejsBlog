d6729b36bf5a01df7fc7cee1b11e7b87131789cb
jacobhistorymaker <historymaker777@hotmail.com>
2020-11-14 12:01:56 +0200

    create partials folder

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res){
 
+  res.render("home", {startingContent: homeStartingContent});
+});
 
 
 
