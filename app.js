377689036bdfd357ed36e7c632006c300c8513ea
Nikos <nikolaos.trigyrakis@intrasoft-intl.com>
2021-02-16 08:47:59 +0200

    Make the application store posts persistently in blogDB (mongoDB)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,67 +17,72 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const postSchema = {
+    title: String,
+    content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 let posts = [];
 
 
-app.get("/", function(req, res){
-  res.render("home", {startingContent: homeStartingContent, posts: posts});
+app.get("/", function (req, res) {
+    Post.find({}, function(err, posts){
+        res.render("home", {
+            startingContent: homeStartingContent,
+            posts: posts
+        });
+    })
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutParagraph: aboutContent});
+app.get("/about", function (req, res) {
+    res.render("about", {aboutParagraph: aboutContent});
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactParagraph: contactContent});
+app.get("/contact", function (req, res) {
+    res.render("contact", {contactParagraph: contactContent});
 });
 
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
     res.render("compose");
 });
 
-app.post("/compose", function(req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
+app.post("/compose", function (req, res) {
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody
+    });
 
-  posts.push(post);
-  res.redirect("/");
+    post.save(function(err){
+        if(!err){
+            res.redirect("/");
+        }
+    });
 });
 
-app.get("/posts/:postName", function(req, res){
-  let found = false;
-  let requestedTitle = req.params.postName;
-  let currentTitle;
-  let matchTitle;
-  let matchBody;
+app.get("/posts/:postId", function (req, res) {
+    let found = false;
+    const requestedPostId = req.params.postId;
 
-  posts.forEach(function(post){
-    currentTitle = _.lowerCase([string=post.title]);
-    requestedTitle = _.lowerCase([string=req.params.postName]);
-    if(currentTitle === requestedTitle){
-      matchBody = post.content;
-      matchTitle = post.title;
-      found = true;
-    }
-  });
+    Post.findOne({_id:requestedPostId}, function(err,post){
+        if(!err){
+            res.render("post", {
+                title: post.title,
+                content: post.content
+            });
+        }
+    });
 
-  if (found) {
-      // console.log("Match found");
-      res.render("post", {currentPostTitle: matchTitle, currentPostBody: matchBody});
-  } else {
-    // console.log("Match not found");
-  }});
+});
 
 
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
